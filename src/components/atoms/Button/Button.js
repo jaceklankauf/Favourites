972d723e5d68d1a0a8e39ed1a6da9e4bb36335d2
jaceklankauf <jacek.lankauf@gmail.com>
2020-06-04 23:11:17 +0200

    FA-04 WIP

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,19 +1,21 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: hsl(55, 100%, 73%);
-  width: ${({ width }) => width || '220px'};
+  padding: 0;
+  background-color: ${({ theme }) => theme.primary};
+  width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: hsl(55, 0%, 83%);
+      background-color: hsl(0, 0%, 90%);
+      width: 105px;
       height: 30px;
       font-size: 10px;
     `}
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import Button from 'components/Button';
-import GlobalStyle from '../../theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import Button from 'components/atoms/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
+import Input from 'components/atoms/Input/Input';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello fuckin world</h1>
-    <Button>close / save</Button>
-    <Button secondary width="105px">
-      remove
-    </Button>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello fuckin world</h1>
+        <Button>close / save</Button>
+        <Button secondary width="105px">
+          remove
+        </Button>
+        <Input search placeholder="search" />
+      </>
+    </ThemeProvider>
   </div>
 );
 
